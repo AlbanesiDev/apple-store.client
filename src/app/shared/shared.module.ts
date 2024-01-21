fff4c385d95c6cc5b99acbe96ddeeeec9308ffcb
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-01-21 01:35:00 -0300

    feat: :sparkles: Shared folder

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ControlErrorsPipe } from './pipes/control-error.pipe';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, ControlErrorsPipe],
+  imports: [CommonModule, RouterModule, TranslateModule, FontAwesomeModule],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    TranslateModule,
+    ControlErrorsPipe,
+  ],
 })
 export class SharedModule {}
