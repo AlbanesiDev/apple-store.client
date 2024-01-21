32b246e203c9e69d777192286cfb6a5bf64d2634
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-01-21 01:34:11 -0300

    feat: :sparkles: Footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,5 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FooterSection } from '../../models/footer.interface';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+} from '@angular/core';
+import { FooterSection } from '../../public/models/footer.interface';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
@@ -8,124 +13,24 @@ import { FooterSection } from '../../models/footer.interface';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
-  //== Mock Data ==
-  public list: FooterSection[] = [
-    {
-      sections: [
-        {
-          title: 'Shop and Learn',
-          items: [
-            'Store',
-            'Mac',
-            'iPad',
-            'iPhone',
-            'Watch',
-            'AirPods',
-            'TV & Home',
-            'AirTag',
-            'Accessories',
-            'Gift Cards',
-          ],
-        },
-        {
-          title: 'Apple Wallet',
-          items: ['Wallet', 'Apple Card', 'Apple Pay', 'Apple Cash'],
-        },
-      ],
-    },
-    {
-      sections: [
-        {
-          title: 'Account',
-          items: ['Manage Your Apple ID', 'Apple Store Account', 'iCloud.com'],
-        },
-        {
-          title: 'Entertainment',
-          items: [
-            'Apple One',
-            'Apple TV+',
-            'Apple Music',
-            'Apple Arcade',
-            'Apple Fitness+',
-            'Apple News+',
-            'Apple Podcasts',
-            'Apple Books',
-            'App Store',
-          ],
-        },
-      ],
-    },
-    {
-      sections: [
-        {
-          title: 'Apple Store',
-          items: [
-            'Find a Store',
-            'Genius Bar',
-            'Today at Apple',
-            'Apple Camp',
-            'Apple Store App',
-            'Certified Refurbished',
-            'Apple Trade in',
-            'Financing',
-            'Carrier Deals at Apple',
-            'Order Status',
-            'Shopping Help',
-          ],
-        },
-      ],
-    },
-    {
-      sections: [
-        {
-          title: 'For Business',
-          items: ['Apple and Business', 'Shop for Business'],
-        },
-        {
-          title: 'For Education',
-          items: ['Apple and Education', 'Shop for K-12', 'Shop for College'],
-        },
-        {
-          title: 'For Healthcare',
-          items: [
-            'Apple in Healthcare',
-            'Health on Apple Watch',
-            'Health Records on iPhone',
-          ],
-        },
-        {
-          title: 'For Government',
-          items: ['Shop for Government', 'Shop for Veterans and Military'],
-        },
-      ],
-    },
-    {
-      sections: [
-        {
-          title: 'Apple Values',
-          items: [
-            'Accessibility',
-            'Education',
-            'Environment',
-            'Inclusion and Diversity',
-            'Privacy',
-            'Racial Equity and Justice',
-            'Supplier Responsibility',
-          ],
-        },
-        {
-          title: 'About Apple',
-          items: [
-            'Newsroom',
-            'Apple Leadership',
-            'Career Opportunitiesities',
-            'Investors',
-            'Ethics & Compliance',
-            'Events',
-            'Contact Apple',
-          ],
-        },
-      ],
-    },
-  ];
+  public list: FooterSection[] = [];
+  public links: string[] = [];
+  public copyright!: string;
+  public text!: string;
+
+  // Temporary region (awaiting  backend)
+  public region: string = 'United States';
+
+  constructor(
+    private translateService: TranslateService,
+    private cdr: ChangeDetectorRef
+  ) {
+    this.translateService.get('footer').subscribe((res: FooterSection[]) => {
+      this.list = res;
+      this.links = res[5].links;
+      this.copyright = res[5].copyright;
+      this.text = res[5].text;
+      this.cdr.markForCheck();
+    });
+  }
 }
