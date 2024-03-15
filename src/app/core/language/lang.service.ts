286e7b5ea5c1818ca98c5016ccf6001eb47f3f21
joaquin albanesi <joaquinalbanesidev@gmail.com>
2024-03-15 02:21:25 -0300

    feat: Language service.

--- src/app/core/language/lang.service.ts
+++ src/app/core/language/lang.service.ts
@@ -1,11 +1,28 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable, PLATFORM_ID, inject } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { TranslateService } from "@ngx-translate/core";
+import { CountryDataService } from "../services/country-data.service";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class ServiceNameService {
+  private translateService = inject(TranslateService);
+  private countryDataService = inject(CountryDataService);
+  private effectiveLang: string | undefined;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-  getUserLanguage() {
+  public initializeLanguage(): void {
+    const browserLang = this.translateService.getBrowserLang();
+    const lang = this.countryDataService.currentLenguageSig();
+    this.effectiveLang = browserLang?.match(/en|fr|ar/) ? browserLang : "en";
+    if (isPlatformBrowser(this.platformId)) {
+      const savedLang = localStorage.getItem("userLanguage");
+      this.effectiveLang = savedLang || (browserLang?.match(/en|es|fr/) ? browserLang : "en");
+    }
+    this.translateService.use(this.effectiveLang);
+
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem("userLanguage", lang);
+    }
   }
 }
